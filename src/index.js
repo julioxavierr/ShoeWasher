f64511ece3e1cad2e64b5a2c707cffc1dd304873
Julio Xavier <julioxavierr@gmail.com>
2018-04-17 17:08:32 -0300

    Project source

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default () => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>Shoe Washer</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
