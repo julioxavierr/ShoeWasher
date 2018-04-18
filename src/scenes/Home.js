a539597bd501db45a9729c5bffb42c66fe2b00bc
Julio Xavier <julioxavierr@gmail.com>
2018-04-18 19:22:12 -0300

    Button that redirects to List scene

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -1,10 +1,15 @@
 import React from 'React';
+import { Button } from 'react-native';
 import { SceneWrapper, SceneDescription } from '@components/Scene';
+import { Actions } from 'react-native-router-flux';
 
-export const Home = () => {
+const Home = () => {
   return (
     <SceneWrapper>
       <SceneDescription>Shoe Washer</SceneDescription>
+      <Button onPress={() => Actions.list()} title="Log in" color="#841584" />
     </SceneWrapper>
   );
 };
+
+export default Home;
