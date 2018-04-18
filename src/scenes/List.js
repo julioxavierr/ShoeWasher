498f42fb96ab089b4b5819a3d9a961fdc333b20c
Julio Xavier <julioxavierr@gmail.com>
2018-04-18 19:48:35 -0300

    Alert confirmation before popping List scene

--- src/scenes/List.js
+++ src/scenes/List.js
@@ -1,12 +1,21 @@
 import React from 'React';
-import { Button } from 'react-native';
+import { Button, Alert } from 'react-native';
 import { SceneWrapper, SceneDescription, NavBar } from '@components/Scene';
 import { Actions } from 'react-native-router-flux';
 
 const List = () => {
+  const _alert = () => {
+    Alert.alert(
+      'Log out',
+      'Are you sure you want to log out?',
+      [{ text: 'Yes', onPress: () => Actions.pop() }, { text: 'Cancel' }],
+      { cancelable: false },
+    );
+  };
+
   return (
     <SceneWrapper>
-      <NavBar title="Wash list" />
+      <NavBar title="Wash list" onPressBack={() => _alert()} />
       <SceneDescription>FlatList</SceneDescription>
       <Button
         onPress={() => Actions.wash()}
