8a438e178ee1f62ce2c5b97dddbce16c39550c2b
Julio Xavier <julioxavierr@gmail.com>
2018-04-18 19:21:11 -0300

    List scene

---
+++ src/scenes/List.js
@@ -0,0 +1,20 @@
+import React from 'React';
+import { Button } from 'react-native';
+import { SceneWrapper, SceneDescription, NavBar } from '@components/Scene';
+import { Actions } from 'react-native-router-flux';
+
+const List = () => {
+  return (
+    <SceneWrapper>
+      <NavBar title="Wash list" />
+      <SceneDescription>FlatList</SceneDescription>
+      <Button
+        onPress={() => Actions.detail()}
+        title="Present detail"
+        color="#841584"
+      />
+    </SceneWrapper>
+  );
+};
+
+export default List;
