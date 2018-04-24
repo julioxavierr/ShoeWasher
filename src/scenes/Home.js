99a9bc7044d7ffbec6f59756ba38d4bbf0cafcb0
Julio Xavier <julioxavierr@gmail.com>
2018-04-24 19:41:17 -0300

    Login form

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -1,15 +1,58 @@
-import React from 'React';
+import React, { Component } from 'React';
 import { Button } from 'react-native';
-import { SceneWrapper, SceneDescription } from '@components/Scene';
+import {
+  SceneWrapper,
+  SceneDescription,
+  SceneContent,
+} from '@components/Scene';
 import { Actions } from 'react-native-router-flux';
+import { Form, Item, Input, Label } from 'native-base';
 
-const Home = () => {
-  return (
-    <SceneWrapper>
-      <SceneDescription>Shoe Washer</SceneDescription>
-      <Button onPress={() => Actions.list()} title="Log in" color="#841584" />
-    </SceneWrapper>
-  );
-};
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '', password: '' };
+  }
+
+  _onSubmit() {
+    console.log('Log user', this.state.username);
+    Actions.list();
+  }
+
+  render() {
+    return (
+      <SceneWrapper>
+        <SceneDescription>Shoe Washer</SceneDescription>
+        <SceneContent>
+          <Form>
+            <Item floatingLabel>
+              <Label>Usuário</Label>
+              <Input
+                value={this.state.username}
+                onChangeText={value =>
+                  this.setState({ username: value.replace(/[^a-z0-9._]/g, '') })
+                }
+                autoCapitalize="none"
+                maxLength={25}
+              />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Senha</Label>
+              <Input
+                secureTextEntry
+                onChangeText={value => this.setState({ password: value })}
+              />
+            </Item>
+          </Form>
+        </SceneContent>
+        <Button
+          onPress={() => this._onSubmit()}
+          title="Log in"
+          color="#841584"
+        />
+      </SceneWrapper>
+    );
+  }
+}
 
 export default Home;
