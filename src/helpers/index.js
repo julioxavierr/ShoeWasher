114938a6ee3ee9ed368a10c3dfaed195f9c3c178
Julio Xavier <julioxavierr@gmail.com>
2018-05-11 14:26:01 -0300

    Update request

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,18 +1,23 @@
 import { store } from '../index';
 
-const requestInfo = (token = store.getState().authentication.token) => {
+const requestInfo = (
+  method,
+  body,
+  token = store.getState().authentication.token,
+) => {
   return {
-    method: 'GET',
+    method: method,
     headers: new Headers({
       'Content-type': 'application/json',
       Authentication: token,
     }),
+    body: body,
   };
 };
 
-export const request = url => {
+export const request = (url, method = 'GET', body = '') => {
   const promise = new Promise((resolve, reject) => {
-    fetch(url, requestInfo())
+    fetch(url, requestInfo(method, body))
       .then(response => resolve(response.json()))
       .catch(() => reject());
   });
