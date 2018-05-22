67497a587bf3b2efc87db30c87ff7f564fb86b8a
Julio Xavier <julioxavierr@gmail.com>
2018-05-22 19:48:29 -0300

    Retrieving services from API

--- src/scenes/WashList.js
+++ src/scenes/WashList.js
@@ -1,4 +1,4 @@
-import React from 'React';
+import React, { Component } from 'React';
 import { Alert } from 'react-native';
 import { List, Content } from 'native-base';
 import { SceneWrapper, NavBar } from '@components/Scene';
@@ -8,51 +8,69 @@ import { connect } from 'react-redux';
 import { logout } from '@redux/authentication/actions';
 import { Actions } from 'react-native-router-flux';
 import exit from '@assets/images/exit.png';
-
-const mockedInProgress = [{ id: 2 }, { id: 3 }];
+import { request } from '@helpers';
 
 const mockedFinished = [{ id: 1 }];
 
-const WashList = props => {
-  const _logout = () => {
-    props.dispatch(logout());
-    Actions.pop();
-  };
+class WashList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { services: [] };
+  }
 
-  const _alert = () => {
+  componentWillMount() {
+    this._getServices();
+  }
+
+  _getServices() {
+    request('http://localhost:8000/api/service_order/service_order/')
+      .then(response => this.setState({ services: response }))
+      .catch(() => {
+        throw new Error('Error getting services');
+      });
+  }
+
+  _logout() {
+    this.props.dispatch(logout());
+    Actions.pop();
+  }
+
+  _alert() {
     Alert.alert(
       'SAIR',
       'Tem certeza que quer deslogar?',
-      [{ text: 'Sim', onPress: () => _logout() }, { text: 'Cancelar' }],
+      [{ text: 'Sim', onPress: () => this._logout() }, { text: 'Cancelar' }],
       { cancelable: false },
     );
-  };
+  }
 
-  return (
-    <SceneWrapper>
-      <NavBar
-        title="Lista de lavagens"
-        onPressBack={() => _alert()}
-        icon={exit}
-      />
-      <Content>
-        <List>
-          <ItemHeader>EM ANDAMENTO</ItemHeader>
-          {mockedInProgress.map(item => (
-            <WashItem key={item.id} id={item.id}>
-              Lavagem #{item.id}
-            </WashItem>
-          ))}
-          <ItemHeader>FINALIZADAS</ItemHeader>
-          {mockedFinished.map(item => (
-            <WashItem key={item.id} id={item.id}>
-              Lavagem #{item.id}
-            </WashItem>
-          ))}
-        </List>
-      </Content>
-    </SceneWrapper>
-  );
-};
+  render() {
+    return (
+      <SceneWrapper>
+        <NavBar
+          title="Lista de serviços"
+          onPressBack={() => this._alert()}
+          icon={exit}
+        />
+        <Content>
+          <List>
+            <ItemHeader>EM ANDAMENTO</ItemHeader>
+            {this.state.services.map(item => (
+              <WashItem key={item.id} id={item.id}>
+                Lavagem #{item.id}
+              </WashItem>
+            ))}
+            <ItemHeader>FINALIZADAS</ItemHeader>
+            {mockedFinished.map(item => (
+              <WashItem key={item.id} id={item.id}>
+                Lavagem #{item.id}
+              </WashItem>
+            ))}
+          </List>
+        </Content>
+      </SceneWrapper>
+    );
+  }
+}
 
 export default connect()(WashList);
